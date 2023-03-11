25277be04faa4ed343d3b9305eeb9d5fbabdbb70
Prachi Pathak <pathakprachi2001@gmail.com>
2023-03-11 09:47:05 +0530

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import redirect, render, HttpResponse
+from blog.models import Post, BlogComment
+from django.contrib import messages
+# Create your views here.
+def blogHome(request): 
+    allPosts= Post.objects.all()
+    context={'allPosts': allPosts}
+    return render(request, "blog/blogHome.html", context)
+    
+def blogPost(request, slug): 
+    post=Post.objects.filter(slug=slug).first()
+    comments= BlogComment.objects.filter(post=post)
+    context={'post':post, 'comments': comments, 'user': request.user}
+    return render(request, "blog/blogPost.html", context)
+
+    # return HttpResponse(f'THIS IS BLOG POST:{slug}')
+
+
+
+def postComment(request):
+    if request.method == "POST":
+        comment=request.POST.get('comment')
+        user=request.user
+        postSno =request.POST.get('postSno')
+        post= Post.objects.get(sno=postSno)
+        comment=BlogComment(comment= comment, user=user, post=post)
+        comment.save()
+        messages.success(request, "Your comment has been posted successfully")
+        
+    return redirect(f"/blog/{post.slug}")
+    # return HttpResponse(f'THIS IS BLOG POST:{slug}')
